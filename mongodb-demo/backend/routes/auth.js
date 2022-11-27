08da5490db00493a40a4f2a9aa7092460c847861
techarm <techarmdev@gmail.com>
2022-11-27 17:55:41 +0900

    Adding authentication

--- mongodb-demo/backend/routes/auth.js
+++ mongodb-demo/backend/routes/auth.js
@@ -2,6 +2,8 @@ const Router = require('express').Router;
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 
+const db = require('../db');
+
 const router = Router();
 
 const createToken = () => {
@@ -13,11 +15,28 @@ router.post('/login', (req, res, next) => {
   const pw = req.body.password;
   // Check if user login is valid
   // If yes, create token and return it to client
-  const token = createToken();
-  // res.status(200).json({ token: token, user: { email: 'dummy@dummy.com' } });
-  res
-    .status(401)
-    .json({ message: 'Authentication failed, invalid username or password.' });
+  db.getClient()
+    .db()
+    .collection('users')
+    .findOne({ email: email })
+    .then((user) => {
+      return bcrypt.compare(pw, user.password);
+    })
+    .then((result) => {
+      if (!result) {
+        throw Error();
+      }
+      const token = createToken();
+      res.status(200).json({
+        message: 'Authentication succeeded.',
+        token: token,
+      });
+    })
+    .catch((err) => {
+      res.status(401).json({
+        message: 'Authentication failed, invalid username or password.',
+      });
+    });
 });
 
 router.post('/signup', (req, res, next) => {
@@ -26,19 +45,25 @@ router.post('/signup', (req, res, next) => {
   // Hash password before storing it in database => Encryption at Rest
   bcrypt
     .hash(pw, 12)
-    .then(hashedPW => {
+    .then((hashedPW) => {
       // Store hashedPW in database
-      console.log(hashedPW);
-      const token = createToken();
-      res
-        .status(201)
-        .json({ token: token, user: { email: 'dummy@dummy.com' } });
+      db.getClient()
+        .db()
+        .collection('users')
+        .insertOne({ email: email, password: hashedPW })
+        .then((result) => {
+          const token = createToken();
+          res.status(201).json({ token: token, user: { email: email } });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json({ message: 'Creating the user failed.' });
+        });
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.status(500).json({ message: 'Creating the user failed.' });
     });
-  // Add user to database
 });
 
 module.exports = router;
